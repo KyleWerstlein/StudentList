f2d78067c8bff742dee26d932af9cb878db62a79
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-10-17 10:45:36 -0700

    no more infinite loops and better conditionals

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,3 +1,6 @@
+/*Kyle Werstlein
+10/15/19
+Student List*/
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -5,30 +8,54 @@
 using namespace std;
 
 struct Student {
-  char name[12];
+  char fname[24];
+  char lname[24];
   int  id;
   float gpa;
 };
 
 int main() {
   bool isRunning = true;
-  char choice[6];
-  vector<char> input;
+  int entries = 0;
+  char input[24];
+  vector<Student> students;
+  vector<int>* vectorP;
   while(isRunning) {
+    char choice[6];
     cout << "ADD, DELETE, PRINT, or QUIT" << endl;
-    cin.get(choice, 7);
-    cin.get();
-    if(choice[0] == 'Q' && choice[1] == 'U' && choice[2] == 'I' && choice[3] == 'T') {
+    // cin.get(choice, 7);
+    cin.getline(choice, 6, '\n');
+    //cin.ignore(1, '\n');
+    if(strcmp(choice, "QUIT") == 0) {
       isRunning = false;
     }
-    else if(choice[0] == 'A' && choice[1] == 'D' && choice[2] == 'D') {
+    else if(strcmp(choice, "ADD") == 0) {
       cout << "ADD" << endl;
-      cout << "What is the name of the student?" << endl;
+      cout << "What is the first name of the student?" << endl;
+      cin.clear();
+      cin.get(input, 25);
+      cin.get();
+      students.push_back(Student());
+      for(int i = 0; i < strlen(input); i++) {
+	students[entries].fname[i] = input[i];
+      }
+      cout << "What is the last name of the student?" << endl;
+      cin.get(input, 25);
+      cin.get();
+      for(int i = 0; i < strlen(input); i++) {
+	students[entries].lname[i] = input[i];
+      }
+      cout << "What is the student's ID?" << endl;
+      cin >> students[entries].id;
+      cout << "What is the student's GPA?" << endl;
+      cin >> students[entries].gpa;
+      cin.ignore(255, '\n');
+      entries++;
     }
-    else if(choice[0] == 'D' && choice[1] == 'E' && choice[2] == 'L' && choice[3] == 'E' && choice[4] == 'T' && choice[5] == 'E') {
+    else if(strcmp(choice, "DELETE") == 0) {
       cout << "DELETE" << endl;
     }
-    else if(choice[0] == 'P' && choice[1] == 'R' && choice[2] == 'I' && choice[3] == 'N' && choice[4] == 'T') {
+    else if(strcmp(choice, "PRINT") == 0) {
       cout << "PRINT" << endl;
     }
   }
