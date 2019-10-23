2484ec9cd56ae2d14f54974d3f2b48874cab2add
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-10-23 10:42:37 -0700

    turn in

--- StudentList.cpp
+++ StudentList.cpp
@@ -8,26 +8,26 @@ Student List*/
 using namespace std;
 
 struct Student {
-  char fname[24];
-  char lname[24];
-  int  id;
+  char fname[24]; // first name
+  char lname[24]; // last name
+  int  id; // student id
   float gpa;
 };
 
 int main() {
-  bool isRunning = true;
+  bool isRunning = true; // not global variables
   int entries = 0;
   int idDelete = 0;
   char input[24];
   vector<Student> students;
-  vector<int>* vectorP;
+  vector<Student*> vectorP;
   while(isRunning) {
     cout << "ADD, DELETE, PRINT, or QUIT" << endl;
-    cin.getline(input, 7, '\n');
+    cin.getline(input, 7, '\n'); // 7 characters because "DELETE" is the biggest word with 6 letters and a "\n"
     if(strcmp(input, "QUIT") == 0) {
-      isRunning = false;
+      isRunning = false; // escape loop
     }
-    else if(strcmp(input, "ADD") == 0) {
+    else if(strcmp(input, "ADD") == 0) { // take all the inputs and put them in a new Student object.
       cout << "ADD" << endl;
       cout << "What is the first name of the student?" << endl;
       cin.get(input, 25);
@@ -47,9 +47,12 @@ int main() {
       cout << "What is the student's GPA?" << endl;
       cin >> students[entries].gpa;
       cin.ignore(255, '\n');
-      entries++;
+      entries++; // keep track of how many students there are
+      Student * ptr;
+      ptr = &students[entries];
+      vectorP.push_back(ptr);
     }
-    else if(strcmp(input, "DELETE") == 0) {
+    else if(strcmp(input, "DELETE") == 0) { // remove a student from the vector by using their student id
        cout << "Enter the ID of the student you want to delete." << endl;
        cin >> idDelete;
        for(int i = 0; i < students.size(); i++) {
@@ -60,7 +63,7 @@ int main() {
        }
        cin.ignore(255, '\n');
     }
-    else if(strcmp(input, "PRINT") == 0) {
+    else if(strcmp(input, "PRINT") == 0) { // print out all the data in the students vector
       for(int i = 0; i < students.size(); i++) {
 	cout << endl;
 	cout << "First name: " << students[i].fname << endl;
